02f1629baa3c93434f8f8fd7c38a34fb7bbad332
Angel <86angel.zz@gmail.com>
2024-08-03 16:33:15 -0600

    create_kit_name_kit_test.py

--- create_kit_name_kit_test.py
+++ create_kit_name_kit_test.py
@@ -27,46 +27,46 @@ def negative_assert_code_400(kit_body, auth_token):
     assert response.status_code == 400
 
 def test_create_kit_1_letter_in_name_get_success_response():
-    auth_token = get_new_user_token("Cesari")
+    auth_token = get_new_user_token("Andrea")
     kit_body = get_kit_body("a")
     positive_assert(kit_body, auth_token)
 
 def test_create_kit_511_letter_in_name_get_success_response():
-    auth_token = get_new_user_token("Cesari")
+    auth_token = get_new_user_token("Andrea")
     kit_body = get_kit_body("AbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdAbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabC")
     positive_assert(kit_body, auth_token)
 
 def test_create_kit_special_caracter_in_name_get_succes_response():
-    auth_token = get_new_user_token("Cesari")
+    auth_token = get_new_user_token("Andrea")
     kit_body = get_kit_body("№%@',")
     positive_assert(kit_body, auth_token)
 
 def test_create_kit_0_letter_in_name_get_error_response():
-    auth_token = get_new_user_token("Cesari")
+    auth_token = get_new_user_token("Andrea")
     kit_body = get_kit_body("")
     negative_assert_code_400(kit_body, auth_token)
 
 def test_create_kit_numbers_in_name_get_succes_response():
-    auth_token = get_new_user_token("Cesari")
+    auth_token = get_new_user_token("Andrea")
     kit_body = get_kit_body("123")
     positive_assert(kit_body, auth_token)
 
 def test_create_kit_512_letter_in_name_get_error_response():
-    auth_token = get_new_user_token("Cesari")
+    auth_token = get_new_user_token("Andrea")
     kit_body = get_kit_body("AbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdAbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcD")
     negative_assert_code_400(kit_body, auth_token)
 
 def test_create_kit_with_spaces_in_name_get_succes_response():
-    auth_token = get_new_user_token("Cesari")
+    auth_token = get_new_user_token("Andrea")
     kit_body = get_kit_body("A aaa")
     positive_assert(kit_body, auth_token)
 
 def test_create_kit_no_parameter_get_error_response():
-    auth_token = get_new_user_token("Cesari")
+    auth_token = get_new_user_token("Andrea")
     kit_body = {} 
     negative_assert_code_400(kit_body, auth_token)
 
 def test_create_kit_type_number_get_error_response():
-    auth_token = get_new_user_token("Cesari")
+    auth_token = get_new_user_token("Andrea")
     kit_body = get_kit_body(123)
     negative_assert_code_400(auth_token, kit_body)
