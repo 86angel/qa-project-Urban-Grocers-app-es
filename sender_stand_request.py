791482a88ee1207895a2a268b86e2386992ff623
Angel <86angel.zz@gmail.com>
2024-08-03 20:20:37 -0600

    Update sender_stand_request.py
    Corrección al comentario: Te recomiendo eliminar el método get_users_table de tu archivo sender_stand_request.py, ya que no se está utilizando y genera un error de compilación. Es importante mantener nuestra suite de pruebas lo más limpia posible.

--- sender_stand_request.py
+++ sender_stand_request.py
@@ -19,7 +19,3 @@ def post_new_user_kit(kit_body, auth_token):
     return requests.post(configuration.URL_SERVICE + configuration.KITS_PATH,
                          json=kit_body,
                          headers=headers)
-
-# OBTENER lista de USUARIO EXISTENTES
-def get_users_table():
-    return requests.get(configuration.URL_SERVICE + configuration.USERS_TABLE_PATH)
