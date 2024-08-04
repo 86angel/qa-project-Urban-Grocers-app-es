2777af116285f6350e2a4e8646b7e7de7ce3702b
Angel <86angel.zz@gmail.com>
2024-08-04 02:23:10 -0600

    Update sender_stand_request.py
    Corrección al comentario:
    
    En el archivo sender_stand_request.py, cuando se manda ejecutar el método post_new_user se deben utilizar los headers que están en el archivo data.py (data.headers). El token de autorización no se usa en este método, ya que aquí es donde apenas se genera.
    
    Línea 6 y 7

--- sender_stand_request.py
+++ sender_stand_request.py
@@ -1,15 +1,12 @@
 import requests
 import configuration
-
+import data
 
 # CREACIÓN de un NUEVO USUARIO
-def post_new_user(body, auth_token):
-    headers = {
-        "Authorization": f"Bearer {auth_token}"
-    }
+def post_new_user(body):
     return requests.post(configuration.URL_SERVICE + configuration.CREATE_USER_PATH,
                          json=body,
-                         headers=headers)
+                         headers=data.headers)
 
 # CREACIÓN de un NUEVO KIT de usuario
 def post_new_user_kit(kit_body, auth_token):
@@ -19,3 +16,7 @@ def post_new_user_kit(kit_body, auth_token):
     return requests.post(configuration.URL_SERVICE + configuration.KITS_PATH,
                          json=kit_body,
                          headers=headers)
+
+# OBTENER lista de USUARIO EXISTENTES
+def get_users_table():
+    return requests.get(configuration.URL_SERVICE + configuration.USERS_TABLE_PATH)
